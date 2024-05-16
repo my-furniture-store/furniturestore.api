724dda325a74208385c56a576afddfaa261fd7a5
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2024-05-16 14:21:48 +0300

    configured api project to work with application and infrastructure layers

--- FurnitureStore.API/Program.cs
+++ FurnitureStore.API/Program.cs
@@ -1,9 +1,15 @@
+using FurnitureStore.Application;
+using FurnitureStore.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var config = builder.Configuration;
 config.AddEnvironmentVariables("FurnitureStoreAPI_");
 
 builder.Services.AddControllers();
+
+builder.Services.AddApplication()
+                .AddInfrastructure(config);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +23,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.Services.RunMigarions();
 
 app.UseHttpsRedirection();
 
