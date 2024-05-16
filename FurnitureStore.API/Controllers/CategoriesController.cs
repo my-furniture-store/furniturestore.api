68d3d57dcafcbb19676fe327dfc33fbdf3f7dce4
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2024-05-16 14:41:09 +0300

    Added ErrorOr and MediatR packages

---
+++ FurnitureStore.API/Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureStore.API.Controllers;
+
+[Route("api/[controller]")]
+public class CategoriesController : ApiController
+{
+    private readonly ISender _mediator;
+
+    public CategoriesController(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+}
