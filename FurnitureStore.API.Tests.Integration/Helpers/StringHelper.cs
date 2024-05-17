5439a9b4c9c7cb54ae5efa8344a5fda4ff1bad5f
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2024-05-17 18:37:48 +0300

    added tests for the categories controller

---
+++ FurnitureStore.API.Tests.Integration/Helpers/StringHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace FurnitureStore.API.Tests.Integration.Helpers;
+
+public static class StringHelper
+{
+    public static string Clamplength(this string str, int minLength, int maxLength)
+    {
+        if(str.Length < minLength)
+            return str.PadRight(minLength);
+
+        if(str.Length > maxLength)
+            return str.Substring(0,maxLength);
+
+
+        return str;
+    }
+}
